b5eaa050a9d1361ab6bebc27a39ae61c8ffbd0f9
smile <yourstyle2018@yahoo.com>
2020-10-14 16:32:08 +0800

    Site updated: 2020-10-14 16:32:08

--- js/comment-issue-data.js
+++ js/comment-issue-data.js
@@ -11,7 +11,7 @@ var COMMENT_CACHE_KEY = "commentKey";
 var valine;
 
 // 管理员名称,评论时添加 [博主] 后缀
-var ADMIN_NAME = "yourstyle2018";
+var ADMIN_NAME = "azusr";
 
 function ajaxReqForGitHub(url, authorizationToken, call) {
     $.ajax({
@@ -332,7 +332,7 @@ function loadIssueData(appId, appKey, userName, userRepo, isValine) {
             }
         }
         // console.clear();
-        console.log("~~~~欢迎光临！记得有时间多来看看哦，https://yourstyle2018.github.io/ ~~~~")
+        console.log("~~~~欢迎光临！记得有时间多来看看哦，https://azusr.github.io/ ~~~~")
     }
         ,
         500
